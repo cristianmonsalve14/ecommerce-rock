e89f1206559cc9ae72860b0cf2165a997d3bc86d
cristianMduoc <TU_EMAIL_DE_GITHUB>
2025-10-22 15:38:25 -0300

    se instalaron las dependecias necesarias para realizar pruebas unitarias con jasmine y karma

---
+++ ecommerce-rock-react/karma.conf.js
@@ -0,0 +1,46 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'src/**/*.spec.jsx',
+      'src/**/*.spec.js'
+    ],
+    preprocessors: {
+      'src/**/*.spec.jsx': ['webpack', 'sourcemap'],
+      'src/**/*.spec.js': ['webpack', 'sourcemap']
+    },
+    webpack: {
+      mode: 'development',
+      module: {
+        rules: [
+          {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env',
+                  '@babel/preset-react'
+                ]
+              }
+            }
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['.js', '.jsx']
+      },
+      devtool: 'inline-source-map'
+    },
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    },
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
